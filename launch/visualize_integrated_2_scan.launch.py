ee2bf156c0af2b54827e26ee6ea8a22c3f8e78b6
srb <saurabh.borse@alumni.fh-aachen.de>
2023-12-20 23:31:46 +0100

    updated topic name and rviz config

--- launch/visualize_integrated_2_scan.launch.py
+++ launch/visualize_integrated_2_scan.launch.py
@@ -18,18 +18,18 @@ def generate_launch_description():
             'laser_scan_integrator.rviz')
 
     #general parameter for the integrated laserscan
-    pointCloudTopic = LaunchConfiguration('integratedTopic', default="/robotino3base1/scan")
+    pointCloudTopic = LaunchConfiguration('integratedTopic', default="/robotinobase1/scan")
     pointCloutFrameId = LaunchConfiguration('integratedFrameId', default="laser_link")
     
     #parameter for the first laserscan, feel free to duplicate and rename for other laserscans
-    scanTopic1 = LaunchConfiguration('scanTopic1', default="/robotino/SickLaser_Front_Remaped")
+    scanTopic1 = LaunchConfiguration('scanTopic1', default="/robotinobase1/SickLaser_Front_Remaped")
     laser1XOff = LaunchConfiguration('laser1XOff', default=0.0)
     laser1YOff = LaunchConfiguration('laser1YOff', default=0.0)
     laser1Alpha = LaunchConfiguration('laser1Alpha', default=0.0)
     show1 = LaunchConfiguration('show1', default=True)
 
     #parameter for the second laserscan, feel free to duplicate and rename for other laserscans
-    scanTopic2 = LaunchConfiguration('scanTopic2', default="/robotino/SickLaser_Rear_Remaped")
+    scanTopic2 = LaunchConfiguration('scanTopic2', default="/robotinobase1/SickLaser_Rear_Remaped")
     laser2XOff = LaunchConfiguration('laser2XOff', default=0.0)
     laser2YOff = LaunchConfiguration('laser2YOff', default=0.0)
     laser2Alpha = LaunchConfiguration('laser2Alpha', default=0.0)
