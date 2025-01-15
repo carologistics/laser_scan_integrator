e28d36a2591c6beb473a977666e86f5b940b746d
sambertibus99 <sam.tho.koehler@gmail.com>
2025-01-15 23:32:08 +0100

    mapper: load parameters from yaml instead of tf broadcast

--- laser_scan_mapper/src/mapper.cpp
+++ laser_scan_mapper/src/mapper.cpp
@@ -1,11 +1,13 @@
 #include <rclcpp/rclcpp.hpp>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+// YAML-CPP
+#include <yaml-cpp/yaml.h>
+#include <fstream>
 
 // Custom Msgs
 #include "laser_scan_integrator_msg/msg/line_segments.hpp"
@@ -14,8 +16,22 @@
 using namespace std::chrono_literals;
 
 /**
+ * @brief Struktur zum Speichern der Maschinen-Daten aus YAML.
+ */
+struct MachineInfo
+{
+  std::string name;
+  double x;
+  double y;
+  double z;
+  double qx;
+  double qy;
+  double qz;
+  double qw;
+};
+
+/**
  * @brief Prüft, ob ein gegebener LineSegment zu einer Maschine gehört.
- *        (Platzhalter-Implementierung, wie vorher gezeigt)
  */
 bool belongsToMachine(
   const laser_scan_integrator_msg::msg::LineSegment &segment,
@@ -23,7 +39,7 @@ bool belongsToMachine(
   double position_tolerance,
   double orientation_tolerance)
 {
-  // 1) Mittelpunkte u. Orientierung aus Segment holen
+  // Unverändert aus vorherigem Code übernommen
   const auto &p1 = segment.end_point1;
   const auto &p2 = segment.end_point2;
   
@@ -32,61 +48,45 @@ bool belongsToMachine(
   segment_mid.y = 0.5 * (p1.y + p2.y);
   segment_mid.z = 0.5 * (p1.z + p2.z);
 
-  double segment_bearing = segment.bearing; // Annahme: in rad
-  
-  // 2) Maschine-Pose aus TF extrahieren
   double machine_x = machine_tf.transform.translation.x;
   double machine_y = machine_tf.transform.translation.y;
-  
+
   tf2::Quaternion q_machine;
   tf2::fromMsg(machine_tf.transform.rotation, q_machine);
 
   double roll, pitch, yaw;
   tf2::Matrix3x3(q_machine).getRPY(roll, pitch, yaw);
-  
-  // 3) Distanz zum Mittelpunkt
+
   double dx = segment_mid.x - machine_x;
   double dy = segment_mid.y - machine_y;
-  double dist = std::sqrt(dx*dx + dy*dy);
+  double dist = std::sqrt(dx * dx + dy * dy);
 
   if (dist > position_tolerance) {
     return false;
   }
 
-  // 4) Orientierungs-Differenz
-  double orient_diff = std::fabs(segment_bearing - yaw);
+  double orient_diff = std::fabs(segment.bearing - yaw);
   if (orient_diff > M_PI) {
     orient_diff = 2.0 * M_PI - orient_diff;
   }
-  if (orient_diff > orientation_tolerance) {
-    return false;
-  }
-
-  return true;
+  return orient_diff <= orientation_tolerance;
 }
 
-
 /**
- * @brief Beispiel-Node, der:
- *  1) line_segments abonniert
- *  2) alle Maschinen-Frames [machine1..machine14] aus TF abfragt
- *  3) prüft, ob eine Linie zu einer dieser Maschinen passt
- *  4) ggf. eine korrigierte TF für die Maschine publiziert
+ * @brief MapperNode, der:
+ *   - Maschinen-Daten aus einer YAML-Datei lädt
+ *   - Toleranzen aus einer separaten YAML-Datei lädt
  */
 class MapperNode : public rclcpp::Node
 {
 public:
-  MapperNode()
-  : Node("mapper")
-  , tf_buffer_(this->get_clock())
-  , tf_listener_(tf_buffer_)
-  , tf_broadcaster_(this)
+  MapperNode(const std::string &machines_yaml, const std::string &tolerance_yaml)
+  : Node("mapper"),
+    tf_broadcaster_(this)
   {
-    // Liste der Maschinen-Namen (wie gewünscht nur die Namen!)
-    machine_frame_ids_ = {
-      "machine1", "machine2", "machine3", "machine4", "machine5", "machine6", "machine7",
-      "machine8", "machine9", "machine10","machine11","machine12","machine13","machine14"
-    };
+    // YAML-Dateien laden
+    loadMachinesFromYAML(machines_yaml);
+    loadTolerancesFromYAML(tolerance_yaml);
 
     // Subscriber für die Segmente
     line_segments_sub_ = this->create_subscription<laser_scan_integrator_msg::msg::LineSegments>(
@@ -95,98 +95,89 @@ public:
       std::bind(&MapperNode::lineSegmentsCallback, this, std::placeholders::_1)
     );
 
-    // Toleranz-Parameter
-    position_tolerance_ = this->declare_parameter<double>("position_tolerance", 0.2);
-    orientation_tolerance_ = this->declare_parameter<double>("orientation_tolerance", 0.2);
-
     RCLCPP_INFO(this->get_logger(), "MapperNode gestartet. Abonniert 'line_segments'.");
   }
 
 private:
-  void lineSegmentsCallback(const laser_scan_integrator_msg::msg::LineSegments::SharedPtr msg)
+  void loadMachinesFromYAML(const std::string &yaml_path)
   {
-    // Für jedes Segment schauen wir, ob es zu einer der Maschinen gehören könnte
-    for (const auto & segment : msg->segments)
-    {
-      // 1) Durchlaufe alle Maschinen-Namen
-      for (const auto & machine_name : machine_frame_ids_)
-      {
-        geometry_msgs::msg::TransformStamped machine_tf;
-        try {
-          // z.B. "map" als Ziel-Frame, machine_name als Quell-Frame
-          // Du musst anpassen, welche Frames in deinem System Sinn ergeben.
-          machine_tf = tf_buffer_.lookupTransform(
-            "map",
-            machine_name,
-            tf2::TimePointZero
-          );
-        } catch (tf2::TransformException &ex) {
-          // Falls der TF nicht verfügbar ist, überspringen
-          // (z.B. Maschine existiert gerade nicht)
-          RCLCPP_WARN_THROTTLE(
-            this->get_logger(),
-            *this->get_clock(),
-            2000, // 2 Sek
-            "Fehler beim TF-Lookup von '%s': %s",
-            machine_name.c_str(),
-            ex.what()
-          );
-          continue;
-        }
-
-        // 2) Linie vs. Maschine checken
-        if (belongsToMachine(segment, machine_tf, position_tolerance_, orientation_tolerance_))
-        {
-          // 3) Neue korrigierte TF generieren und publizieren
-          geometry_msgs::msg::TransformStamped corrected_tf;
-          corrected_tf.header.stamp = this->now();
-          corrected_tf.header.frame_id = machine_tf.header.frame_id; 
-          // => z.B. "map"
-
-          // Neuer Child-Frame => z.B. "machine_corrected_<machine_name>"
-          corrected_tf.child_frame_id = "machine_corrected_" + machine_name;
-
-          // Beispiel: wir verschieben um 0.05 in x (reine Demo)
-          corrected_tf.transform.translation.x = machine_tf.transform.translation.x + 0.05;
-          corrected_tf.transform.translation.y = machine_tf.transform.translation.y;
-          corrected_tf.transform.translation.z = machine_tf.transform.translation.z;
-
-          // Orientation übernehmen
-          corrected_tf.transform.rotation = machine_tf.transform.rotation;
-
-          tf_broadcaster_.sendTransform(corrected_tf);
-
-          RCLCPP_INFO(
-            this->get_logger(),
-            "LineSegment passt zu [%s]. Neue TF '%s' veröffentlicht.",
-            machine_name.c_str(),
-            corrected_tf.child_frame_id.c_str()
-          );
-        }
-      } // Ende Maschinen-Schleife
-    } // Ende Segment-Schleife
+    YAML::Node config = YAML::LoadFile(yaml_path);
+    if (config["machines"]) {
+      for (const auto &m : config["machines"]) {
+        MachineInfo info;
+        info.name = m["name"].as<std::string>();
+        info.x    = m["translation"]["x"].as<double>();
+        info.y    = m["translation"]["y"].as<double>();
+        info.z    = m["translation"]["z"].as<double>();
+        info.qx   = m["rotation"]["x"].as<double>();
+        info.qy   = m["rotation"]["y"].as<double>();
+        info.qz   = m["rotation"]["z"].as<double>();
+        info.qw   = m["rotation"]["w"].as<double>();
+        machine_infos_.push_back(info);
+      }
+    }
   }
 
-  // TF
-  tf2_ros::Buffer tf_buffer_;
-  tf2_ros::TransformListener tf_listener_;
-  tf2_ros::TransformBroadcaster tf_broadcaster_;
+  void loadTolerancesFromYAML(const std::string &yaml_path)
+  {
+    YAML::Node config = YAML::LoadFile(yaml_path);
+    if (config["tolerance"]) {
+      position_tolerance_ = config["tolerance"]["position_tolerance"].as<double>();
+      orientation_tolerance_ = config["tolerance"]["orientation_tolerance"].as<double>();
+    }
+  }
 
-  // Subscriber
+  void lineSegmentsCallback(const laser_scan_integrator_msg::msg::LineSegments::SharedPtr msg)
+  {
+    for (const auto &segment : msg->segments) {
+      for (const auto &machine : machine_infos_) {
+        geometry_msgs::msg::TransformStamped machine_tf;
+        machine_tf.header.frame_id = "map";
+        machine_tf.child_frame_id = machine.name;
+        machine_tf.header.stamp = this->now();
+
+        machine_tf.transform.translation.x = machine.x;
+        machine_tf.transform.translation.y = machine.y;
+        machine_tf.transform.translation.z = machine.z;
+
+        machine_tf.transform.rotation.x = machine.qx;
+        machine_tf.transform.rotation.y = machine.qy;
+        machine_tf.transform.rotation.z = machine.qz;
+        machine_tf.transform.rotation.w = machine.qw;
+
+        if (belongsToMachine(segment, machine_tf, position_tolerance_, orientation_tolerance_)) {
+          geometry_msgs::msg::TransformStamped corrected_tf = machine_tf;
+          corrected_tf.child_frame_id = "machine_corrected_" + machine.name;
+          corrected_tf.transform.translation.x += 0.05;
+
+          tf_broadcaster_.sendTransform(corrected_tf);
+          RCLCPP_INFO(this->get_logger(), "LineSegment passt zu [%s]. Neue TF '%s' veröffentlicht.", machine.name.c_str(), corrected_tf.child_frame_id.c_str());
+        }
+      }
+    }
+  }
+
+  tf2_ros::TransformBroadcaster tf_broadcaster_;
   rclcpp::Subscription<laser_scan_integrator_msg::msg::LineSegments>::SharedPtr line_segments_sub_;
 
-  // Parameter
-  double position_tolerance_;
-  double orientation_tolerance_;
-
-  // Nur die Namen der Maschinen-Frames
-  std::vector<std::string> machine_frame_ids_;
+  std::vector<MachineInfo> machine_infos_;
+  double position_tolerance_ = 0.2;
+  double orientation_tolerance_ = 0.2;
 };
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<MapperNode>();
+
+  std::string machines_yaml = "../config/machines.yaml";
+  std::string tolerance_yaml = "../config/tolerance.yaml";
+
+  if (argc > 2) {
+    machines_yaml = argv[1];
+    tolerance_yaml = argv[2];
+  }
+
+  auto node = std::make_shared<MapperNode>(machines_yaml, tolerance_yaml);
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
